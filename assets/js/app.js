bb6a9d3186acd035a9556b16ad456f0ef5dbc092
Nini Ninikelashvili <66223574+Ninikelashvili@users.noreply.github.com>
2023-02-01 23:59:15 +0400

    added menu structure

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,23 @@
+const active = () => {
+    const body = document.querySelector('body')
+    const burger = document.querySelector('.burger')
+    const overlay = document.querySelector('.navigation-wrapper .overlay')
+
+    if(burger) {
+        burger.addEventListener('click', () => {
+            body.classList.toggle('navigation-open')
+        })
+    }
+
+    if(overlay) {
+        overlay.addEventListener('click', () => {
+            body.classList.remove('navigation-open')
+        })
+    }
+}
+
+active()
+
 AOS.init();
 
 
